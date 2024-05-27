d9a40a74845513ec59d73f84321220a14ce31420
anil-g11h <anilraj659@gmail.com>
2024-05-27 08:56:11 +0530

    title update

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <title>Next.js MongoDB App</title>
+        <title>EchoMateLite</title>
         <meta
           key="viewport"
           name="viewport"
