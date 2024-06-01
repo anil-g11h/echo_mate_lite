79e123676f60aaeb6132e49cf4e5c905815986ef
anil-g11h <anilraj659@gmail.com>
2024-06-01 13:55:26 +0530

    fix upload profile pic using s3

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -3,20 +3,28 @@ import { findUserByUsername, updateUserById } from '@/api-lib/db';
 import { auths, validateBody } from '@/api-lib/middlewares';
 import { getMongoDb } from '@/api-lib/mongodb';
 import { ncOpts } from '@/api-lib/nc';
-import { getSession } from '@/api-lib/middlewares/session';
 import { slugUsername } from '@/lib/user';
 import multer from 'multer';
 import nc from 'next-connect';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
+
+const unlinkFile = util.promisify(fs.unlink);
 
 const upload = multer({ dest: '/tmp' });
 const handler = nc(ncOpts);
 
+const s3 = new S3Client({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
 handler.use(...auths);
 
 handler.get(async (req, res) => {
-  console.log('req.userapi', req.user, req.session);
-  const session = await getSession(req, res);
-  console.log('session', session);
   if (!req.user) return res.json({ user: null });
   return res.json({ user: req.user });
 });
@@ -41,6 +49,29 @@ handler.patch(
     const db = await getMongoDb();
 
     let profilePicture;
+    if (req.file) {
+      const fileContent = fs.readFileSync(req.file.path);
+      const fileName = `${Date.now()}_${path.basename(req.file.path)}`;
+      const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: fileName,
+        Body: fileContent,
+        ContentType: req.file.mimetype,
+        ACL: 'public-read',
+      };
+
+      try {
+        const uploadResult = await s3.send(new PutObjectCommand(params));
+        profilePicture = `https://${params.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
+      } catch (err) {
+        console.error('Error uploading to S3', err);
+        res.status(500).json({ error: 'Error uploading file' });
+        return;
+      }
+      // Remove the file from the local filesystem
+      await unlinkFile(req.file.path);
+    }
+
     const { name, bio } = req.body;
 
     let username;
